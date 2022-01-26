e8d20300c91334907624f667c91a497d8e450f4e
Nazar Pechevystyi <nazarpechka@gmail.com>
2022-01-26 20:25:38 +0100

    fix: persisted cart state (localStorage)

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,18 @@
 import { createStore } from "redux";
 import reducers from "./reducers";
 
+const persistedState = localStorage.getItem("store")
+  ? JSON.parse(localStorage.getItem("store"))
+  : {};
+
 const store = createStore(
   reducers,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+  localStorage.setItem("store", JSON.stringify(store.getState()));
+});
 
 export default store;
